f8787a914a05effd57183bdba4489ae80a1c89a5
Jan Carbonell <jcllobet@gmail.com>
2022-03-18 17:53:46 +0100

    removed fa

--- js/main_page.js
+++ js/main_page.js
@@ -3,3 +3,24 @@ const flipTriangle = (e) => {
     //e.target.classList.toggle("arrow-down");
     //e.target.classList.toggle("arrow-up");
 };
+
+const css = (element, property) => {
+    return window.getComputedStyle(element, null).getPropertyValue(property);
+};
+
+const onLoad = () => {
+    let span = document.createElement("span");
+    console.log("Hello World");
+
+    span.className = "fa";
+    span.style.display = "none";
+    document.body.insertBefore(span, document.body.firstChild);
+
+    if (css(span, "font-family") !== "FontAwesome") {
+        // add a local fallback
+        console.log("FontAwesome not found");
+    }
+    document.body.removeChild(span);
+};
+
+window.onload = onLoad();
